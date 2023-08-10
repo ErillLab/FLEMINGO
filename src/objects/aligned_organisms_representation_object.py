379ce5adf0b26ae8b21520445fc0dc18c96c42d8
Elia Mascolo <eliamascolo94@gmail.com>
2023-08-10 17:12:44 -0400

    Now 'annotate_available_connectors' is a method of AlignedOrganismsRepresentation, and the 'connectors_table' is an attribute of AlignedOrganismsRepresentation.

--- src/objects/aligned_organisms_representation_object.py
+++ src/objects/aligned_organisms_representation_object.py
@@ -24,6 +24,9 @@ class AlignedOrganismsRepresentation:
         self.organism1 = []
         self.organism2 = []
         
+        # XXX
+        self.connectors_table = None
+        
         # Organisms IDs
         self.parents_ids = [organism1_id, organism2_id]
         self.children_ids = [None, None]
@@ -54,6 +57,78 @@ class AlignedOrganismsRepresentation:
     
     def set_children_IDs(self, child1_id, child2_id):
         self.children_ids = [child1_id, child2_id]
+    
+    def annotate_available_connectors(self):
+        '''
+        The representations of the aligned parents are lists of symbols.
+        For each possible couple of positions in the representations, this
+        function annotates whether the parents have a connector that connects
+        them.
+        
+        EXAMPLE:
+        In this representations
+        
+            p1_0    p1_1    -       p1_2
+            -       p2_0    p2_1    -   
+        
+        index 0 is liked to index 1 by the first connector of p1 (parent1): the
+        connector that connects recognizer p1_0 with recognizer p1_1.
+        
+        Index 1 is liked to index 3 by the second connector of p1: the
+        connector that connects recognizer p1_1 with recognizer p1_2.
+        
+        Index 1 is liked to index 2 by the only connector of p2: the
+        connector that connects recognizer p2_0 with recognizer p2_1.
+        
+        Parameters
+        ----------
+        parent1_repres : list
+            Representation of parent1 (aligned against parent2).
+        parent2_repres : list
+            Representation of parent2 (aligned against parent1).
+
+        Returns
+        -------
+        connectors_table : 2D list
+            This table stores at row i, column j the connector(s) available to
+            link index i to index j.
+        '''
+        
+        
+        # XXX Code here to catch case when organisms were not set yet
+        n = len(self.organism1)
+        
+        # 2D list where each item is an emtpy list
+        connectors_table = [[ [] for i in range(n)] for j in range(n)]
+        
+        # Each parent representation is coupled with a tag ('p1' or 'p2')
+        parents = [(self.organism1, 'p1'),
+                   (self.organism2, 'p2')]
+        
+        for (org_repr, org_tag) in parents:
+            
+            # Indexes where a recognizer of this parent is present        
+            recogs_indexes = []
+            for idx in range(len(org_repr)):
+                if org_repr[idx] != '-':
+                    recogs_indexes.append(idx)
+            
+            connector_idx = 0
+            for i in range(len(recogs_indexes)-1):
+                
+                left_recog_idx = recogs_indexes[i]
+                right_recog_idx = recogs_indexes[i+1]
+                
+                left_recog_name = org_repr[left_recog_idx]
+                right_recog_name = org_repr[right_recog_idx]
+                
+                if left_recog_name != right_recog_name:                
+                    connector_name = org_tag + '_' + str(connector_idx)
+                    connector_idx += 1
+                    connectors_table[left_recog_idx][right_recog_idx].append(connector_name)
+        
+        # Set the table storing info about available connectors
+        self.connectors_table = connectors_table
 
 
 
--- src/objects/organism_factory.py
+++ src/objects/organism_factory.py
@@ -594,12 +594,13 @@ class OrganismFactory:
         parents_repres = self.get_aligned_parents_repr(par1, par2, reference_dna_seq)
         
         # Table storing info about what connectors are available to cover the possible spans
-        connectors_table = self.annotate_available_connectors(parents_repres)
+        parents_repres.annotate_available_connectors()
         
         # Representation of the two recombined children aligned
         children_repres = self.get_aligned_children_repr(parents_repres, child1._id, child2._id)
         
-        # XXX ...
+        # XXX ... Change this so that the table is stored in children_repres ?
+        connectors_table = parents_repres.connectors_table
         
         # Assemble child 1
         # Write the assembly instructions
@@ -797,74 +798,74 @@ class OrganismFactory:
         
         return pos_to_recog_dict
     
-    def annotate_available_connectors(self, parents_repres):
-        '''
-        The representations of the aligned parents are lists of symbols.
-        For each possible couple of positions in the representations, this
-        function annotates whether the parents have a connector that connects
-        them.
+    # def annotate_available_connectors(self, parents_repres):
+    #     '''
+    #     The representations of the aligned parents are lists of symbols.
+    #     For each possible couple of positions in the representations, this
+    #     function annotates whether the parents have a connector that connects
+    #     them.
         
-        EXAMPLE:
-        In this representations
+    #     EXAMPLE:
+    #     In this representations
         
-            p1_0    p1_1    -       p1_2
-            -       p2_0    p2_1    -   
+    #         p1_0    p1_1    -       p1_2
+    #         -       p2_0    p2_1    -   
         
-        index 0 is liked to index 1 by the first connector of p1 (parent1): the
-        connector that connects recognizer p1_0 with recognizer p1_1.
+    #     index 0 is liked to index 1 by the first connector of p1 (parent1): the
+    #     connector that connects recognizer p1_0 with recognizer p1_1.
         
-        Index 1 is liked to index 3 by the second connector of p1: the
-        connector that connects recognizer p1_1 with recognizer p1_2.
+    #     Index 1 is liked to index 3 by the second connector of p1: the
+    #     connector that connects recognizer p1_1 with recognizer p1_2.
         
-        Index 1 is liked to index 2 by the only connector of p2: the
-        connector that connects recognizer p2_0 with recognizer p2_1.
+    #     Index 1 is liked to index 2 by the only connector of p2: the
+    #     connector that connects recognizer p2_0 with recognizer p2_1.
         
-        Parameters
-        ----------
-        parent1_repres : list
-            Representation of parent1 (aligned against parent2).
-        parent2_repres : list
-            Representation of parent2 (aligned against parent1).
+    #     Parameters
+    #     ----------
+    #     parent1_repres : list
+    #         Representation of parent1 (aligned against parent2).
+    #     parent2_repres : list
+    #         Representation of parent2 (aligned against parent1).
 
-        Returns
-        -------
-        connectors_table : 2D list
-            This table stores at row i, column j the connector(s) available to
-            link index i to index j.
-        '''
+    #     Returns
+    #     -------
+    #     connectors_table : 2D list
+    #         This table stores at row i, column j the connector(s) available to
+    #         link index i to index j.
+    #     '''
         
-        n = len(parents_repres.organism1)
+    #     n = len(parents_repres.organism1)
         
-        # 2D list where each item is an emtpy list
-        connectors_table = [[ [] for i in range(n)] for j in range(n)]
+    #     # 2D list where each item is an emtpy list
+    #     connectors_table = [[ [] for i in range(n)] for j in range(n)]
         
-        # Each parent representation is coupled with a tag ('p1' or 'p2')
-        parents = [(parents_repres.organism1, 'p1'),
-                    (parents_repres.organism2, 'p2')]
+    #     # Each parent representation is coupled with a tag ('p1' or 'p2')
+    #     parents = [(parents_repres.organism1, 'p1'),
+    #                 (parents_repres.organism2, 'p2')]
         
-        for (org_repr, org_tag) in parents:
+    #     for (org_repr, org_tag) in parents:
             
-            # Indexes where a recognizer of this parent is present        
-            recogs_indexes = []
-            for idx in range(len(org_repr)):
-                if org_repr[idx] != '-':
-                    recogs_indexes.append(idx)
+    #         # Indexes where a recognizer of this parent is present        
+    #         recogs_indexes = []
+    #         for idx in range(len(org_repr)):
+    #             if org_repr[idx] != '-':
+    #                 recogs_indexes.append(idx)
             
-            connector_idx = 0
-            for i in range(len(recogs_indexes)-1):
+    #         connector_idx = 0
+    #         for i in range(len(recogs_indexes)-1):
                 
-                left_recog_idx = recogs_indexes[i]
-                right_recog_idx = recogs_indexes[i+1]
+    #             left_recog_idx = recogs_indexes[i]
+    #             right_recog_idx = recogs_indexes[i+1]
                 
-                left_recog_name = org_repr[left_recog_idx]
-                right_recog_name = org_repr[right_recog_idx]
+    #             left_recog_name = org_repr[left_recog_idx]
+    #             right_recog_name = org_repr[right_recog_idx]
                 
-                if left_recog_name != right_recog_name:                
-                    connector_name = org_tag + '_' + str(connector_idx)
-                    connector_idx += 1
-                    connectors_table[left_recog_idx][right_recog_idx].append(connector_name)
-        # Return the table storing info about available connectors
-        return connectors_table
+    #             if left_recog_name != right_recog_name:                
+    #                 connector_name = org_tag + '_' + str(connector_idx)
+    #                 connector_idx += 1
+    #                 connectors_table[left_recog_idx][right_recog_idx].append(connector_name)
+    #     # Return the table storing info about available connectors
+    #     return connectors_table
     
     def get_aligned_children_repr(self, parents_repres, child1_id, child2_id):
         '''
